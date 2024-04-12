c0a8b90019fcc819c5f3b5614a81507d1f0f9936
Cameron <camallan16@gmail.com>
2024-04-12 17:05:37 -0400

    created and implemented main component

---
+++ weather-app/src/components/WeatherComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="weather-container">
+        <div class="weather-wrap">
+            <div class="search-box"> <input type="text" placeholder="Search..." class="search-bar" v-model="query"
+                v-on:keypress="fetchWeather" /> </div>
+        <div class="weather-info" v-if="typeof weather.main!=undefined'">
+            <div class="location-box">
+                <div class="location">{{weather.name}},{{weather.sys.country }} </div>
+                <div class="date">{{ todaysDate() }}</div>
+            </div>
+            <div class="weather-box">
+                <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+                <div class="weather">{{ weather.weather[0].main }}</div>
+                <div class="icon">
+                    <img:src="`${weather_icon}${weather.weather[0].icon}${'@2x.png'}`" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div></template>

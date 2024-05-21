37d2969f25c3d321608e5dc554c616765e523a82
Cameron Allan <camallan16@gmail.com>
2024-05-21 16:13:08 -0400

    Big oopsie doopsie

--- weather-app/src/components/WeatherComponent.vue
+++ weather-app/src/components/WeatherComponent.vue
@@ -1,21 +1,27 @@
-import axios from 'axios';
-
 <script>
+import axios from 'axios';
 export default {
     data() {
         return {
-            api_key: "aa86246b96b03134456dcbb486b0f4c2",
+            api_key: `${process.env.WEATHER_API_KEY}`,
             url_base: "https://api.openweathermap.org/data/2.5/",
             weather_icon: "http://openweathermap.org/img/wn/",
             query: "",
             weather: {},
         };
     },
-    async fetchWeather(e) {
-        if (e.key == "Enter") { let response = await axios.get(`${this.url_base}weather? q=${this.query}&units=metric&APPID=${this.api_key}`); this.setResults(response.data); }
-    }, setResults(returnedResponse) {
-        this.weather = returnedResponse;
-    },
+    methods: {
+        async fetchWeather(e) {
+            
+            if (e.key == "Enter") { let response = await axios.get(`${this.url_base}weather? q=${this.query}&units=metric&APPID=${this.api_key}`); this.setResults(response.data); }
+        }, setResults(returnedResponse) {
+            this.weather = returnedResponse;
+        },
+        todaysDate() {
+            return new Date().toDateString();
+        }
+    }
+
 };
 
 
@@ -27,18 +33,18 @@ export default {
                 <input type="text" placeholder="Search..." class="search-bar" v-model="query"
                     v-on:keypress="fetchWeather" />
             </div>
-            <div class="weather-info" v-if="typeof weather.main != undefined'">
+            <div class="weather-info">
                 <div class="location-box">
                     <div class="location">
-                        {{ weather.name }},{{ weather.sys.country }}
+
                     </div>
                     <div class="date">{{ todaysDate() }}</div>
                 </div>
                 <div class="weather-box">
-                    <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
-                    <div class="weather">{{ weather.weather[0].main }}</div>
+                    <div class="temp">°f</div>
+                    <div class="weather"></div>
                     <div class="icon">
-                        <img:src="`${weather_icon}${weather.weather[0].icon}${'@2x.png'}`" />
+                        <!-- <img :src="`${weather_icon}${weather.weather[0].icon}${'@2x.png'}`" /> -->
                     </div>
                 </div>
             </div>
